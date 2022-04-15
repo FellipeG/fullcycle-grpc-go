77d35924e8d990c355f5f0ac6a2deef54827cec3
FellipeG <fellipeg.rjqoor@gmail.com>
2022-04-15 20:06:46 -0300

    Registrando service

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net"
 
+	"github.com/FellipeG/fullcycle-grpc-go/pb"
+	"github.com/FellipeG/fullcycle-grpc-go/services"
 	"google.golang.org/grpc"
 )
 
@@ -16,6 +18,7 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Could not serve: %v", err)
