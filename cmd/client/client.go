7abd78e0155f613e0b0bb3094ba9b3908c4d55a7
FellipeG <fellipeg.rjqoor@gmail.com>
2022-04-15 21:20:30 -0300

    Implementando client para o server stream

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"log"
 
 	"github.com/FellipeG/fullcycle-grpc-go/pb"
@@ -17,7 +19,8 @@ func main() {
 	defer connection.Close()
 
 	client := pb.NewUserServiceClient(connection)
-	AddUser(client)
+	AddUserVerbose(client)
+	//AddUser(client)
 
 
 }
@@ -36,5 +39,33 @@ func AddUser(client pb.UserServiceClient) {
 	}
 
 	log.Println(res)
+
+}
+
+func AddUserVerbose(client pb.UserServiceClient) {
+
+	req := &pb.User{
+		Id: "0",
+		Name: "Fellipe",
+		Email: "fellipeg.rjqoor@gmail.com",
+	}
+
+	resStream, err := client.AddUserVerbose(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	for {
+		stream, err := resStream.Recv()
+		if err == io.EOF {
+			break;
+		}
+
+		if err != nil {
+			log.Fatalf("Could not receive the message: %v", err)
+		}
+
+		fmt.Println("Status: ", stream.Status , " - ", stream.GetUser())
+	}
 
 }
