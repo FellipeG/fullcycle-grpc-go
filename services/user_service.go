8c371e0ca01ee13d028eb1e92fde46eecbe07c65
FellipeG <fellipeg.rjqoor@gmail.com>
2022-04-15 21:03:03 -0300

    Implementando server stream

--- services/user_service.go
+++ services/user_service.go
@@ -3,12 +3,14 @@ package services
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/FellipeG/fullcycle-grpc-go/pb"
 )
 
 //type UserServiceServer interface {
 //	AddUser(context.Context, *User) (*User, error)
+//	AddUserVerbose(*User, UserService_AddUserVerboseServer) error
 //	mustEmbedUnimplementedUserServiceServer()
 //}
 
@@ -30,4 +32,45 @@ func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error)
 		Name: req.GetName(),
 		Email: req.GetEmail(),
 	}, nil
+}
+
+func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer) err {
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Init",
+		User: &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Inserting",
+		User: &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "User has been inserted",
+		User: &pb.User{
+			Id: "123",
+			Name: req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Completed",
+		User: &pb.User{
+			Id: "123",
+			Name: req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	return nil
 }
